1dd1d240096aa2f385b271257cf85601598c69e6
KaviyashreeG <160014046+KaviyashreeG@users.noreply.github.com>
2026-03-10 10:36:25 +0530

    rectified

--- src/components/reminder/ReminderMananger.js
+++ src/components/reminder/ReminderMananger.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+import useEffect from 'react';
 
 const ReminderMananger = ({ events, onComplete, notify }) => {
     useEffect(() => {
